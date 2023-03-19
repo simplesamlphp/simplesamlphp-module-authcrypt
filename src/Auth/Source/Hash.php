179ab67dfc25d7f859ef95c2a7a41786a8649875
Tim van Dijen <tvdijen@gmail.com>
2023-03-19 22:36:02 +0100

    Fix codesniffer issues

--- src/Auth/Source/Hash.php
+++ src/Auth/Source/Hash.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SimpleSAML\Module\authcrypt\Auth\Source;
 
 use Exception;
--- src/Auth/Source/Htpasswd.php
+++ src/Auth/Source/Htpasswd.php
@@ -6,6 +6,8 @@
  * @package SimpleSAMLphp
  */
 
+declare(strict_types=1);
+
 namespace SimpleSAML\Module\authcrypt\Auth\Source;
 
 use Exception;
